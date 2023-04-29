194f3f0d165befe83104b715d6ec62273dee8e0f
Harsh Sharma (aka Nish) <thhat.guy.is@gmail.com>
2023-04-29 01:29:34 -0400

    Update Progress.jsx

--- Group-Project-CS-WEB59000-Team1-master2 2 3/src/Progress.jsx
+++ Group-Project-CS-WEB59000-Team1-master2 2 3/src/Progress.jsx
@@ -51,20 +51,32 @@ function UserProgress({ user }) {
             </td>
           </tr>
           <tr>
-            <td>GPA</td>
-            <td>
-              <ProgressBar now={user.GPA} label={`${user.GPA}`} />
-            </td>
-          </tr>
-          <tr>
             <td>i20 Progress</td>
             <td>
               <ProgressBar now={user.i_20_progress} label={`${user.i_20_progress}%`} />
             </td>
           </tr>
           <tr>
+            <td>GPA</td>
+            <td>
+              <ProgressBar now={user.GPA} label={`${user.GPA}`} />
+            </td>
+          </tr>
+          <tr>
+            <td>GRE</td>
+            <td>{user.GRE}</td>
+          </tr>
+          <tr>
             <td>Admitted</td>
             <td>{user.Admitted}</td>
+          </tr>
+          <tr>
+            <td>Country</td>
+            <td>{user.country}</td>
+          </tr>
+          <tr>
+            <td>Education Domain</td>
+            <td>{user.domain}</td>
           </tr>
         </tbody>
       </table>
@@ -75,6 +87,8 @@ function UserProgress({ user }) {
 function Progress() {
   const [users, setUsers] = useState([]);
   const [sortOrder, setSortOrder] = useState('asc');
+  const [greSortOrder, setGreSortOrder] = useState('asc');
+  const [selectedCountry, setSelectedCountry] = useState('');
 
   useEffect(() => {
     fetch('https://web-dev-server-wm7w.onrender.com/api/users')
@@ -85,13 +99,28 @@ function Progress() {
         } else if (sortOrder === 'desc') {
           data.sort((a, b) => b.GPA - a.GPA);
         }
+
+        if (greSortOrder === 'asc') {
+          data.sort((a, b) => a.GRE - b.GRE);
+        } else if (greSortOrder === 'desc') {
+          data.sort((a, b) => b.GRE - a.GRE);
+        }
+
+        if (selectedCountry) {
+          data = data.filter((user) => user.country === selectedCountry);
+        }
+
         setUsers(data);
       })
       .catch((error) => console.log(error));
-  }, [sortOrder]);
+  }, [sortOrder, greSortOrder, selectedCountry]);
 
   const handleSortOrderChange = (event) => {
     setSortOrder(event.target.value);
+  };
+
+  const handleCountryChange = (event) => {
+    setSelectedCountry(event.target.value);
   };
 
   return (
@@ -99,20 +128,38 @@ function Progress() {
       <nav>
         <Header />
       </nav>
-     <div className="toggle-container">
-      <div className="center">
-        <label htmlFor="sortOrder">Sort Order GPA:</label>
-        <select id="sortOrder" value={sortOrder} onChange={handleSortOrderChange}>
-          <option value="asc">Ascending</option>
-          <option value="desc">Descending</option>
-        </select>
+      <div className="toggle-container">
+        <div className="center">
+          <label htmlFor="sortOrder">Sort Order GPA:</label>
+          <select id="sortOrder" value={sortOrder} onChange={handleSortOrderChange}>
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+        <div className="center">
+          <label htmlFor="greSortOrder">Sort Order GRE:</label>
+          <select id="greSortOrder" value={greSortOrder} onChange={(event) => setGreSortOrder(event.target.value)}>
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+        <div className="center">
+          <label htmlFor="country">Select Country:</label>
+          <select id="country" value={selectedCountry} onChange={handleCountryChange}>
+            <option value="">All</option>
+            <option value="Australia">Australia</option>
+            <option value="USA">USA</option>
+            <option value="India">India</option>
+          </select>
         </div>
       </div>
+
       {users.map((user) => (
         <UserProgress key={user._id} user={user} />
       ))}
     </div>
   );
 }
+
 
 export default Progress;
